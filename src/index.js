d71a01ec3b32cab41c44f5d94eaaa31084075289
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-02-23 16:39:29 +0100

    Second part - routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Router, Route, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
+import Navigation from './presentational/navigation.component';
+import DevTools from './DevTools';
+import routes from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store = {store}>
+		<Router history={hashHistory} routes={routes} />
+		<DevTools />
+	</Provider>,
+	document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
